08f7d002c338624b5034aada635e17cb0b68ffb1
sharjeelyunus <sharjeel924@gmail.com>
2020-05-30 01:59:54 +0500

    📦 NEW: Customize Theme

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,8 +12,63 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReuseableCard(
+                    color: Color(0xff1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReuseableCard(
+                    color: Color(0xff1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReuseableCard(
+              color: Color(0xff1D1E33),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReuseableCard(
+                    color: Color(0xff1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReuseableCard(
+                    color: Color(0xff1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReuseableCard extends StatelessWidget {
+  ReuseableCard({@required this.color});
+
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
